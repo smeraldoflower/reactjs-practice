f3102fefcdfc0f6bba24f2414e8dc8f33daa9b75
BlueMcFlurry <57018533+BlueMcFlurry@users.noreply.github.com>
2021-02-18 22:44:17 -0600

    added more PP03 files

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function Contact() {
     return (
-        <div>
+        <div className="Contact">
+            <h1>Contact ME</h1>
+            <br/>
             <h2>Phone: +1 234 567 8910</h2>
-            
+            <h2>Email: myemail@email.com</h2>
+            <h2>LinkedIn: <a>https://www.linkedin.com/in/iffatnusaibamahmood/</a></h2>
         </div>
     )
 }
